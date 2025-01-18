a97a7a95b623716769de253fe9ebf9afd274ed1b
Stavros Argyroudis <stavrosargyroud@gmail.com>
2025-01-18 20:25:49 +0200

    1st real commit with some logic on the cards states and some css corrections

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,60 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import warrior_card from './assets/warrior_card.png';
+import weapons_card from './assets/weapons_card.png';
 import experience_card from './assets/experience_card.png';
-import warrior_card from './assets/warrior_card.webp';
-import weapons_card from './assets/weapons_card.webp';
-import './App.css';
+import './App.scss';
+
+const STATES = {
+  START: 'start',
+  WARRIOR: 'warrior',
+  WEAPONS: 'weapons',
+  EXPERIENCE: 'experience',
+};
+
+const STATE_ORDER = [STATES.START, STATES.WARRIOR, STATES.WEAPONS, STATES.EXPERIENCE];
 
 function App() {
+  const [currentStep, setCurrentStep] = useState(STATES.START);
+
+  const handleImageClick = (id: string) => {
+    // Get the current index and the index of the clicked state
+    const currentIndex = STATE_ORDER.indexOf(currentStep);
+    const nextIndex = STATE_ORDER.indexOf(id);
+
+    // Only update the state if the clicked state is the same or ahead in the hierarchy
+    if (nextIndex > currentIndex) {
+      setCurrentStep(id);
+      console.log(`state changed to ${id}`);
+    }
+  };
+
+  useEffect(() => {
+    console.log('🚀 ~ currentStep:', currentStep);
+  }, [currentStep]);
+
   return (
     <div className='root'>
       <div className='backgroundContainer' />
-      {/* <img
-        src={backgroundImage}
-        className='backgroundImage'
-        alt='background  image with medieval style'
-      />
-      <img
-        src={backgroundImage}
-        className='backgroundImage'
-        alt='background  image with medieval style'
-      />
-      <img
-        src={backgroundImage}
-        className='backgroundImage'
-        alt='background  image with medieval style'
-      /> */}
-
-      {/*      <div>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+      <div className='imageContainer'>
+        <img
+          onClick={() => handleImageClick(STATES.WARRIOR)}
+          src={warrior_card}
+          className='cards'
+          alt='a warrior card image'
+        />
+        <img
+          onClick={() => handleImageClick(STATES.WEAPONS)}
+          src={weapons_card}
+          className='cards nextCard'
+          alt='a weapons card image'
+        />
+        <img
+          onClick={() => handleImageClick(STATES.EXPERIENCE)}
+          src={experience_card}
+          className='cards nextCard'
+          alt='a experience card image'
+        />
       </div>
-      <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p> */}
     </div>
   );
 }
