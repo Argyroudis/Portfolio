2a8d1cb8d618b09d9f9a488e5cd8e3b96eb6776b
Stavros Argyroudis <stavros@addc.network.arxnet>
2025-01-18 19:39:14 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import experience_card from './assets/experience_card.png';
+import warrior_card from './assets/warrior_card.webp';
+import weapons_card from './assets/weapons_card.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className='root'>
+      <div className='backgroundContainer' />
+      {/* <img
+        src={backgroundImage}
+        className='backgroundImage'
+        alt='background  image with medieval style'
+      />
+      <img
+        src={backgroundImage}
+        className='backgroundImage'
+        alt='background  image with medieval style'
+      />
+      <img
+        src={backgroundImage}
+        className='backgroundImage'
+        alt='background  image with medieval style'
+      /> */}
+
+      {/*      <div>
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p> */}
+    </div>
+  );
+}
+
+export default App;
