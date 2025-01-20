9810c53d930d433f1a49dca17cd455c844d5335c
Stavros Argyroudis <stavrosargyroud@gmail.com>
2025-01-20 21:29:16 +0200

    adding some content on formal screen and some ideas with comments

--- src/routes/FormalHome/FormalHome.tsx
+++ src/routes/FormalHome/FormalHome.tsx
@@ -1,5 +1,16 @@
 import { useNavigate } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { Button, IconButton } from '@mui/material';
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineOppositeContent,
+  // TimelineDot,
+} from '@mui/lab';
+import { Typography } from '@mui/material';
+import { FaReact, FaGitAlt } from 'react-icons/fa';
 import { HOME_PATH } from '../HomeScreen';
 import { languageToken } from '@/assets/messages';
 import './style.scss';
@@ -18,10 +29,75 @@ export function FormalHome() {
   return (
     <div className='root'>
       <div className='Header'>
-        <div className='Name'>{languageToken.fullName}</div>
+        {/* <div className='Name'>{languageToken.fullName}</div>
         <div className='jobTitle'>{languageToken.jobTitle}</div>
-        <div className='bio'>{languageToken.bio}</div>
+        <div className='bio'>{languageToken.bio}</div> */}
+        <Timeline position='alternate'>
+          {/* Timeline Item 1 */}
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography
+                variant='body2'
+                color='#e0e0e0'
+                sx={{
+                  fontSize: 'x-large',
+                  fontWeight: 800,
+                }}
+              >
+                {languageToken.fullName}
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <IconButton>
+                <FaReact color='#61DAFB' size={40} />
+              </IconButton>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Typography
+                variant='h6'
+                color='#ffc107'
+                component='span'
+                sx={{
+                  fontSize: 'larger',
+                  fontWeight: 800,
+                }}
+              >
+                {languageToken.jobTitle}
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
+
+          {/* Timeline Item 2 */}
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant='body2' color='#e0e0e0'>
+                {languageToken.what}
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <IconButton>
+                <FaGitAlt color='#F14E32' size={40} />
+              </IconButton>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Typography
+                variant='h6'
+                component='span'
+                color='#ffc107'
+                sx={{
+                  fontSize: 'medium',
+                  fontWeight: 600,
+                }}
+              >
+                {languageToken.bio}
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
       </div>
+
       <div className='bottomContainer'>
         <div className='buttonContainer'>
           <Button
