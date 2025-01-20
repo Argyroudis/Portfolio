ad0b241f30e42e155b4d40994fa579a0223dfc75
Stavros Argyroudis <stavrosargyroud@gmail.com>
2025-01-20 20:24:49 +0200

    start working on the formal page

--- src/routes/FormalHome/FormalHome.tsx
+++ src/routes/FormalHome/FormalHome.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 import { HOME_PATH } from '../HomeScreen';
+import { languageToken } from '@/assets/messages';
 import './style.scss';
 
 export const FORMAL_HOME_PATH = '/FormalHome/';
@@ -16,6 +17,11 @@ export function FormalHome() {
 
   return (
     <div className='root'>
+      <div className='Header'>
+        <div className='Name'>{languageToken.fullName}</div>
+        <div className='jobTitle'>{languageToken.jobTitle}</div>
+        <div className='bio'>{languageToken.bio}</div>
+      </div>
       <div className='bottomContainer'>
         <div className='buttonContainer'>
           <Button
